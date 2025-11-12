1ec884c93d0aa6187b071c0b5649c4a1be080a48
Prince Vuha <princevuha-1113@users.noreply.github.com>
2025-11-12 23:03:03 +0000

    fix padding for join screen

--- prodev-mobile-app-0x05/styles/index.tsx
+++ prodev-mobile-app-0x05/styles/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+        paddingHorizontal: 20,
         backgroundColor: '#fff',
         position: 'relative'
     },
@@ -115,4 +115,5 @@ const styles = StyleSheet.create({
 
 export {
     styles
-}
+};
+
